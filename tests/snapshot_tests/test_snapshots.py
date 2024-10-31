6c519c8d51dd9157b27968b8fc426d88246bfaec
Ted Conbeer <tconbeer@users.noreply.github.com>
2024-10-31 19:09:09 +0000

    fix: add test for null rep

--- tests/snapshot_tests/test_snapshots.py
+++ tests/snapshot_tests/test_snapshots.py
@@ -25,6 +25,10 @@ def test_datatable_no_render_markup(snap_compare: Callable) -> None:
     assert snap_compare(SNAPSHOT_APPS_DIR / "data_table_no_render_markup.py")
 
 
+def test_datatable_null_mixed_cols(snap_compare: Callable) -> None:
+    assert snap_compare(SNAPSHOT_APPS_DIR / "data_table_null_mixed_cols.py")
+
+
 def test_datatable_range_cursor_render(snap_compare: Callable) -> None:
     press = ["right", "down", "shift+right", "shift+down", "shift+down"]
     assert snap_compare(SNAPSHOT_APPS_DIR / "data_table_range_cursor.py", press=press)
